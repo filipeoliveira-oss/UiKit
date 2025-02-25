4b0ae03afc0e2a0195cb95ee237aaa3120a6c1ad
Filipe Oliveira <filipeoliveira084@gmail.com>
2025-02-24 21:46:22 -0300

    feat: implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Checkbox from './components/checkBox/checkBox'
-
 function App() {
 
-  const [value,setValue] = useState(false)
-
   return (
-    <div className='w-dvw h-dvh flex items-center justify-center'>
-      <Checkbox setValue={(e) => setValue(e)} value={value} format='square' />
+    <div>
+      this is the main page
     </div>
   )
 }
